1ac12d32a8375fa10acd037297d0a34f9a5db2a1
alexandre souza <c.alexandresouza@yahoo.com>
2025-10-01 11:09:17 -0300

    truncate list invoices add]

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,12 @@
 module InvoicesHelper
+  # Trunca uma lista de atributos de objetos com "..." se passar do limite
+  def truncated_list(collection, attribute, limit = 3)
+    return "" if collection.blank?
+
+    values = collection.map(&attribute)
+    display_values = values.first(limit)
+    result = display_values.join(", ")
+    result += "…" if values.size > limit
+    result
+  end
 end
