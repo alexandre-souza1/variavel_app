bbb14ca5133ceb42d6e12b34b112ea250d058c76
alexandre souza <c.alexandresouza@yahoo.com>
2025-10-27 11:01:03 -0300

    bug fix too many lines

--- app/javascript/controllers/response_controller.js
+++ app/javascript/controllers/response_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["details"]
+  static targets = ["details", "photoInput", "commentInput"]
 
   connect() {
     console.log("Controller conectado para:", this.element)
@@ -10,10 +10,22 @@ export default class extends Controller {
   showDetails() {
     console.log("Mostrando detalhes para:", this.element)
     this.detailsTarget.classList.remove("d-none")
+
+    // habilita os campos quando o usuário marca NOK
+    if (this.hasPhotoInputTarget) this.photoInputTarget.disabled = false
+    if (this.hasCommentInputTarget) this.commentInputTarget.disabled = false
   }
 
   hideDetails() {
     console.log("Ocultando detalhes para:", this.element)
     this.detailsTarget.classList.add("d-none")
+
+    // desabilita os campos quando o usuário marca OK
+    if (this.hasPhotoInputTarget) this.photoInputTarget.disabled = true
+    if (this.hasCommentInputTarget) this.commentInputTarget.disabled = true
+
+    // limpa os valores (opcional, mas evita envio acidental)
+    if (this.hasCommentInputTarget) this.commentInputTarget.value = ""
+    if (this.hasPhotoInputTarget) this.photoInputTarget.value = null
   }
 }
