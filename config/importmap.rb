f5fee55142f1a835db6740d78f2c2291b02a84d5
AÃlexandre Souza <155571649+alexandre-souza1@users.noreply.github.com>
2026-04-11 09:07:03 -0300

    teams function commit 6 bootstrap fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,5 @@ pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.300
 pin "sortablejs" # @1.15.7
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
