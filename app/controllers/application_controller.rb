de3d543ececb862a3c29c44041ab9919707215ad
alexandre souza <155571649+alexandre-souza1@users.noreply.github.com>
2026-08-07 11:00:45 -0300

    general visual improvements #navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,34 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :secondary_nav
+
+  def secondary_nav
+    return "admin_nav" if finance_module?
+    return "fleet_nav" if fleet_module?
+  end
+
   protected
+
+  def finance_module?
+    controller_path.in?([
+      "invoices",
+      "suppliers",
+      "admin/budget_categories",
+      "admin/cost_centers"
+    ])
+  end
+
+  def fleet_module?
+    controller_path.in?([
+      "fleet_availabilities",
+      "action_plans",
+      "routines",
+      "dashboards",
+      "placas_por_setor",
+      "plates",
+    ])
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
