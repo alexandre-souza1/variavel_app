cd43a687f082d47ec3c637e40e5da803f29d07d0
alexandre souza <155571649+alexandre-souza1@users.noreply.github.com>
2026-09-14 15:01:46 -0300

    see public chat everywhere on consultas

--- app/helpers/public_variable_chat_helper.rb
+++ app/helpers/public_variable_chat_helper.rb
@@ -1,4 +1,11 @@
 module PublicVariableChatHelper
+  def public_variable_chat_widget
+    render "public_variable_chat/widget",
+      identity: PublicVariableIdentity.from_session(session[:public_variable_chat]),
+      history: Array(session.dig(:public_variable_chat, "history")),
+      open: session.delete(:public_variable_chat_open)
+  end
+
   def clean_public_variable_chat_text(text)
     text.to_s
       .gsub(/\\n/, "\n")
